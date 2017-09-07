c2c05e30e0901654fc0917b8e82a3af9236df5e5
joesan <codeintheopen@gmail.com>
2017-09-07 18:02:33 +0200

    trying dropdown menu

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {disableDebugTools} from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
+
+// This line below is to overcome a bug that I encountered with the ng4-material-dropdown
+// https://stackoverflow.com/questions/46097266
+disableDebugTools();
 
 const bootstrapPromise =  platformBrowserDynamic().bootstrapModule(AppModule);
 
